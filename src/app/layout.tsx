043e2c31fa50f79cabd6a8bdbbeeb65b9980fa1b
ArmanSingh24 <armansingh6692@gmail.com>
2026-03-21 00:27:43 +0530

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,11 @@ import {
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import { Meowl } from "@/components/Meowl";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -14,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
       <head>
         <script
           dangerouslySetInnerHTML={{
